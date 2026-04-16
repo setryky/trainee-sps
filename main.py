0d324685bc36ce7f1d5bb4de05fe7f561c1afd7f
Victhor Alexandre <xvicthor.alexandre@gmail.com>
2026-04-16 19:48:47 -0300

    .

--- main.py
+++ main.py
@@ -2,15 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def modulacao(fc, fm, fs, t):
+def gerar_portadora(fc, fs, t):
 
+    return portadora
+
+def modulacao(portadora, mensagem, fs, t):
+    
     return m_t
 
+def demodulacao(m_t, fs, t):
+
+    m_t_hat = retificacao(m_t)
+    s_t = low-pass-filter(m_t_hat)
+
+    return s_t
 
 
 
+def retificacao(r_t):
+    return np.abs(r_t)
+
+def low-pass-filter()
 
 
 
 if __name__ == '__main__':
 
+    portadora = gerar_portadora(fc, fs, t)
+    
